1ee2e1129a4c25add11825e3fd31b2eba78e426c
gigi <gigichakaberia@gmail.com>
2024-10-02 17:39:04 +0400

    final product

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from datetime import datetime
+
+directory = 'files'
+
+filenames = os.listdir(directory)
+
+for filename in filenames:
+    filepath = os.path.join(directory, filename)
+
+    with open(filepath) as file:
+        content = file.read()
+        words = content.split()
+        word_count = len(words)
+
+    day = datetime.now().strftime("%A")
+
+    new_filename = f'{filename[:4]}-{word_count}-{day}.txt'
+
+    new_filepath = os.path.join(directory, new_filename)
+    os.rename(filepath, new_filepath)
