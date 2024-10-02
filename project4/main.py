6e8cc40879af30719a626d6fd128febe7c9ca4e1
gigi <gigichakaberia@gmail.com>
2024-10-02 20:16:15 +0400

    added: opening csv file

--- project4/main.py
+++ project4/main.py
@@ -2,6 +2,10 @@ import pandas
 
 df = pandas.read_excel("project4/europe.xlsx")
 
-print(df)
-
 df.to_json("project4/europe.json", orient='records')
+
+df.to_csv('project4/europe.csv', index=False)
+
+with open('project4/europe.csv') as file:
+    print(next(file), end='')
+    print(next(file))
