2768b2de421519121489af87ff34db474de07793
gigi <gigichakaberia@gmail.com>
2024-10-04 17:10:12 +0400

    new projects

--- project16/main.py
+++ project16/main.py
@@ -4,4 +4,3 @@ def convert_excel_to_json(excel_file):
     df = pandas.read_excel(excel_file)
     json_data = df.to_json(orient='records')
     return json_data
-
--- project4/main.py
+++ project4/main.py
@@ -3,9 +3,3 @@ import pandas
 df = pandas.read_excel("project4/europe.xlsx")
 
 df.to_json("project4/europe.json", orient='records')
-
-df.to_csv('project4/europe.csv', index=False)
-
-with open('project4/europe.csv') as file:
-    print(next(file), end='')
-    print(next(file))
