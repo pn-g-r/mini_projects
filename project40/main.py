ebeb2fae280266508b39338365c34a23a6240f62
gigi <gigichakaberia@gmail.com>
2024-10-17 20:12:58 +0400

    comm

--- project40/main.py
+++ project40/main.py
@@ -7,9 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
+# Create an instance of Chrome Options
 chrome_options = Options()
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_options.add_argument("--start-fullscreen")  # This argument opens Chrome in full-screen
 
+# Initialize the WebDriver with the full-screen option
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 driver.get('https://demoqa.com/login')
 
@@ -22,7 +25,6 @@ password_field.send_keys('PythonStudent123$')
 
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable(login_button))
 login_button.click()
-
 
 time.sleep(3)
 driver.quit()
